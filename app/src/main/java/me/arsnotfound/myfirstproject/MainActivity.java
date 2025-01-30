b651c401dbd46ce01f6890357c010b32888864aa
Arslan 'ArsNotFound' Sakhapov <arslan.sakhapov@yandex.ru>
2025-01-30 20:18:16 +0500

    Initial commit

---
+++ app/src/main/java/me/arsnotfound/myfirstproject/MainActivity.java
@@ -0,0 +1,43 @@
+package me.arsnotfound.myfirstproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import me.arsnotfound.myfirstproject.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    private ActivityMainBinding binding;
+
+    private final ActivityResultLauncher<String> loginActivityLauncher = registerForActivityResult(new LoginActivity.LoginActivityContract(), creds -> {
+        Log.i(TAG, "Got credentials from login: " + creds.getUsername() + " " + creds.getPassword());
+    });
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        EdgeToEdge.enable(this);
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main, (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        binding.startLoginActivity.setOnClickListener(view ->
+                loginActivityLauncher.launch("Test string pls ignore")
+        );
+    }
+}
